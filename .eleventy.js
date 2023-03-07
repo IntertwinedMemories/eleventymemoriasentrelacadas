da93a3f2a13bb346db62e1c10a38ab3afcf4292f
Guilherme Zühlke O'Connor <guioconnor@guioconnor.com>
2023-03-07 20:51:33 +0000

    Date format for posts

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/images");
 
+
+  /** Converts the given date string to ISO8601 format. */
+  const toISOString = (dateString) => new Date(dateString).toISOString();
+  eleventyConfig.addFilter('toISOString', toISOString);
+
+  const dateFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formatDate = (date) => date.toLocaleDateString("en-GB", dateFormatOptions);
+  eleventyConfig.addFilter('formatDate', formatDate);
+
   return {
     dir: {
       input: "src",
